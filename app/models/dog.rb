f75c48e9cb80d8db07e49f5ef11839ea9a196fb5
Trever Andersen <trever.r.andersen@gmail.com>
2017-11-24 18:19:05 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,9 +1,9 @@
 class Dog
   attr_accessor :name, :age
   attr_reader :breed
-  def initialize(attributes)
-    attributes.each do |key, value|
-      instance_variable_set("@#{key}", value)
-    end
+  def initialize(name, breed, age)
+    @name = name
+    @breed = breed
+    @age = age
   end
 end
