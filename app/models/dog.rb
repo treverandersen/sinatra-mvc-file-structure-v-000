6b9ba82b2724dfbd8bdd6a5922f8687a23df3bac
Trever Andersen <trever.r.andersen@gmail.com>
2017-11-24 18:32:26 +0000

    automatically backed up by learn

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,13 +1,14 @@
 class Dog
   attr_accessor :name, :age
   attr_reader :breed
-  
+
   def initialize(name, breed, age)
     @name = name
     @breed = breed
     @age = age
     @@all = []
   end
+  
   def self.all
     @@all
   end
